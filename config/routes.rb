2f06cea722b2cd1b0ced0b9d4d17a3816b13c3ae
Jonasz Kądziela <jonaszkadziela@o2.pl>
2018-07-05 16:41:25 +0200

    Generated controllers & defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "landing#show"
+  resources :profile, only: %i(show edit) do
+    resources :sections, except: %i(index show) do
+      resources :items, except: %i(index show)
+    end
+  end
 end
